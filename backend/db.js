1e08870d22b60df0dd73b61907ca71108c4a5e98
rittika-22 <ijarin422@gmail.com>
2026-02-23 13:34:08 +0600

    dbconnection

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../.env' }); // Look for .env one level up
+require('dotenv').config(); 
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
